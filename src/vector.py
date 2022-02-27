c9157bea264ae70424d51e8cad56eb7672a60496
Vihaan Tarale <taralevihaan@gmail.com>
2022-02-27 21:30:37 +0530

    completed

---
+++ src/vector.py
@@ -0,0 +1,36 @@
+import math
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def get_mag(self):
+        return math.sqrt((self.x ** 2) + (self.y ** 2))
+
+    def set_mag(self, new_mag):
+        self.x = self.x * new_mag / self.get_mag()
+        self.y = self.y * new_mag / self.get_mag()
+
+    def sub(self, vector):
+        self.x = self.x - vector.x
+        self.y = self.y - vector.x
+    
+    def norm(self):
+        self.x = self.x * 1 / self.get_mag()
+        self.y = self.y * 1 / self.get_mag()
+
+    def add(self, vector):
+        self.x = self.x + vector.x
+        self.y = self.y + vector.x
+
+    def __sub__(self, vector):
+        self.x = self.x - vector.x
+        self.y = self.y - vector.x
+    
+    def __add__(self, vector):
+        self.x = self.x + vector.x
+        self.y = self.y + vector.x
+
+    def dot(self, vector):
+        dot = self.x(vector.x) + self.y(vector.y)
